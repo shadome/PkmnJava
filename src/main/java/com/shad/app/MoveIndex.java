7c6d98bc3500684e9f661978faa9e3be3fcd1e83
shadome <marceau.imag@gmail.com>
2018-06-14 00:33:26 +0200

    Init

---
+++ src/main/java/com/shad/app/MoveIndex.java
@@ -0,0 +1,15 @@
+
+public enum MoveIndex {
+	TOPLEFT(0),
+	TOPRIGHT(1),
+	BOTLEFT(2),
+	BOTRIGHT(3);
+	
+	private int index;
+	private MoveIndex(int index) {
+		this.index = index;
+	}
+	public int getIndex() {
+		return this.index;
+	}
+}
